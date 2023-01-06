9dcf62873875f8173ca0e513b7b687ae993a0a22
Kacper <kacper0724@poczta.fm>
2023-01-06 16:54:23 +0100

    add two method add and substract

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Calculator calculator = new Calculator();
+
+        double sumAdd = calculator.add(10, 11.5);
+        double sumSubstract = calculator.substract(10.5, 10);
+
+        System.out.println(sumAdd);
+        System.out.println(sumSubstract);
+    }
+}
+
+class Calculator {
+    public double add(double a, double b) {
+        return a +b;
+    }
+
+    public double substract(double a, double b) {
+        return a - b;
     }
 }
